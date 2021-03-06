f5a8a48afb153bc02b4cdeb5df6d0dde39031193
Rohit Mittal <mittalr685@gmail.com>
2021-03-06 18:30:13 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,76 @@
+import java.util.*;
+import java.util.ArrayList;
+
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Welcome to Bingo. It is still a WIP. If the board has duplicates, make sure to rerun the program, but hopefully it does not.");
+    int[][] board = new int[4][4];
+    ArrayList<Integer> Nums = new ArrayList<Integer>();
+    createBoard(Nums, board);
+    printBoard(board);
+   
+    
+  }
+
+public static ArrayList<Integer> createBoard(ArrayList<Integer> Nums, int[][] board)
+{
+  int index = 0;
+  while(Nums.size() < 16)
+  {
+    Nums.add((int) (Math.random()*(100- 10)) + 10);
+    Nums = removeDup(Nums);
+  }
+  for(int r = 0; r < board.length; r++)
+  {
+    for(int c = 0; c < board[r].length; c++)
+    {
+      board[r][c] = Nums.get(index);
+      index++;
+    }
+  }
+ 
+  return Nums;
+
+}
+
+
+public static ArrayList<Integer> removeDup(ArrayList<Integer> Nums) 
+    { 
+  
+        // Create a new ArrayList 
+        ArrayList<Integer> newList = new ArrayList<Integer>(); 
+  
+        // Traverse through the first list 
+        for (Integer element : Nums) { 
+  
+            // If this element is not present in newList 
+            // then add it 
+            if (!newList.contains(element)) { 
+  
+                newList.add(element); 
+            } 
+        } 
+  
+        // return the new list 
+        return newList; 
+    } 
+
+
+
+
+
+public static int[][] printBoard(int[][] board)
+{
+ for(int r = 0; r < board.length; r++)
+    {
+        for(int c = 0; c < board[r].length; c++)
+        {
+          System.out.print(board[r][c] + " | ");
+        }
+        System.out.println();
+    }
+    return board;
+}
+
+
+}
